fe1b9f1f9813126e654d4c1e0a1615e196f91983
CristianM <crism.klee@gmail.com>
2024-06-06 20:22:00 -0300

    Valida el token

--- src/main/java/med/voll/apiMedic/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/apiMedic/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,6 +13,9 @@ import java.rmi.RemoteException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -22,6 +26,7 @@ public class SecurityFilter extends OncePerRequestFilter {
         }
         token = token.replace("Bearer ", "");
         System.out.println(token);
+        System.out.println(tokenService.getSubject(token));     // Este usuario tiene sesion?
         filterChain.doFilter(request, response);
     }
 }
