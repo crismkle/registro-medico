b01ab49c1ee59bfe8c496f2186697f79cbc3ea8d
CristianM <crism.klee@gmail.com>
2024-06-06 20:04:39 -0300

    Vefica el token

--- src/main/java/med/voll/apiMedic/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/apiMedic/infra/security/SecurityFilter.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.rmi.RemoteException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -15,7 +16,11 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // Obtener el token del header
-        var token = request.getHeader("Authorization").replace("Bearer ", "");
+        var token = request.getHeader("Authorization");
+        if (token == null || token == ""){
+            throw new RemoteException("El token enviado no es válido");
+        }
+        token = token.replace("Bearer ", "");
         System.out.println(token);
         filterChain.doFilter(request, response);
     }
